453a678028c7a193fd1ea6d8f5abcea1b13b3d5a
Chris Shea <cmshea@gmail.com>
2013-08-01 17:40:28 -0400

    FizzBuzz

--- src/understanding_computation/chapter_six/fizz.clj
+++ src/understanding_computation/chapter_six/fizz.clj
@@ -73,8 +73,96 @@
                   (((IF ((IS_LESS_OR_EQUAL n) m))
                     (fn [x] (((f ((SUBTRACT m) n)) n) x))) m))))))
 
+(def EMPTY ((PAIR TRUE) TRUE))
+(def UNSHIFT (fn [l]
+               (fn [x]
+                 ((PAIR FALSE) ((PAIR x) l)))))
+(def IS_EMPTY LEFT)
+(def FIRST (fn [l]
+             (LEFT (RIGHT l))))
+(def REST (fn [l]
+            (RIGHT (RIGHT l))))
+
+(def RANGE (Z (fn [f]
+                (fn [m]
+                  (fn [n]
+                    (((IF ((IS_LESS_OR_EQUAL m) n))
+                      (fn [x]
+                        (((UNSHIFT ((f (INCREMENT m)) n)) m) x)))
+                     EMPTY))))))
+
+(def FOLD (Z (fn [f]
+               (fn [l]
+                 (fn [x]
+                   (fn [g]
+                     (((IF (IS_EMPTY l))
+                       x)
+                      (fn [y]
+                        (((g (((f(REST l)) x) g)) (FIRST l)) y)))))))))
+
+(def MAP (fn [k]
+           (fn [f]
+             (((FOLD k) EMPTY) (fn [l]
+                                 (fn [x]
+                                   ((UNSHIFT l) (f x))))))))
+
+(def TEN ((MULTIPLY TWO) FIVE))
+(def B TEN)
+(def F (INCREMENT B))
+(def I (INCREMENT F))
+(def U (INCREMENT I))
+(def ZED (INCREMENT U))
+
+(def FIZZ ((UNSHIFT ((UNSHIFT ((UNSHIFT ((UNSHIFT EMPTY) ZED)) ZED)) I)) F))
+(def BUZZ ((UNSHIFT ((UNSHIFT ((UNSHIFT ((UNSHIFT EMPTY) ZED)) ZED)) U)) B))
+(def FIZZBUZZ ((UNSHIFT ((UNSHIFT ((UNSHIFT ((UNSHIFT BUZZ) ZED)) ZED)) I)) F))
+
+(def DIV (Z (fn [f]
+              (fn [m]
+                (fn [n]
+                  (((IF ((IS_LESS_OR_EQUAL n) m))
+                    (fn [x]
+                      ((INCREMENT ((f ((SUBTRACT m) n)) n)) x)))
+                   ZERO))))))
+
+(def PUSH (fn [l]
+            (fn [x]
+              (((FOLD l) ((UNSHIFT EMPTY) x)) UNSHIFT))))
+
+(def TO_DIGITS (Z (fn [f]
+                    (fn [n]
+                      ((PUSH
+                        (((IF
+                           ((IS_LESS_OR_EQUAL n) (DECREMENT TEN)))
+                          EMPTY)
+                         (fn [x] ((f ((DIV n) TEN)) x))))
+                       ((MOD n) TEN))))))
+
 (defn to-integer [p]
   ((p inc) 0))
 
 (defn to-boolean [p]
   (((IF p) true) false))
+
+(defn to-array [a]
+  (loop [a a ret []]
+    (if (to-boolean (IS_EMPTY a))
+      ret
+      (recur (REST a) (conj ret (FIRST a))))))
+
+(defn to-char [c]
+  (nth "0123456789BFiuz" (to-integer c)))
+
+(defn to-string [s]
+  (apply str (map to-char (to-array s))))
+
+(comment
+  (map to-string
+       (to-array
+        ((MAP ((RANGE ONE) HUNDRED))
+         (fn [n]
+           (((IF (IS_ZERO ((MOD n) FIFTEEN)))
+             FIZZBUZZ)
+            (((IF (IS_ZERO ((MOD n) THREE)))
+              FIZZ)
+             (((IF (IS_ZERO ((MOD n) FIVE))) BUZZ) (TO_DIGITS n)))))))))
